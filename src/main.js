39ec98603515dac3afb74e92070fd7a1a7bc3d49
JOLLA99 <yoon6148@daum.net>
2024-09-26 10:53:41 +0900

    feat : KBF-9-충돌해결

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+import App from "./App.vue";
+import router from "./router";
+
+import "./styles/tailwind.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 app.use(createPinia());
 app.use(router);
