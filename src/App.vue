b5402b091b8b804d5dda636c2cc002efdc5ce878
yoondabin <yundabin0608@gmail.com>
2024-09-30 20:23:37 +0900

    feat: KBF-5 요소 재배치

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,51 @@ import IconSideBar from './components/IconSideBar.vue';
 </script>
 
 <template>
-  <div id="app">
     <div class="sidebar">
       <!-- 사이드바 메뉴 -->
       <IconSideBar />
     </div>
-    <RouterView />
-  </div>
+
+    <div class="main-content">
+      <!-- 페이지 내용은 여기서 로드됨 -->
+      <RouterView />
+    </div>
 </template>
+
+<style>
+.sidebar {
+  width: 2rem;
+  position: fixed; 
+  top: 0;
+  left: 0;
+  height: 100vh;
+}
+
+.sidebar .logo {
+  display: block;
+  margin-bottom: 20px;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar ul li {
+  margin: 10px 0;
+}
+
+.sidebar ul li a {
+  text-decoration: none;
+  color: #333;
+}
+
+.main-content {
+  flex-grow: 1;
+  padding: 20px;
+  background-color: #fff;
+  overflow-y: auto;
+  margin-left: 5rem;
+}
+
+</style>
