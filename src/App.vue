ad79025fbbbb5bb7ce6419a0ae69bf948a811dbf
김시완 <rkdlqdh112@naver.com>
2024-10-01 17:41:20 +0900

    feat: KBF-13 rebase 충돌 병합

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import IconSideBar from "./components/IconSideBar.vue";
+</script>
+
 <template>
   <div id="app">
-    <div class="sidebar">
-      <!-- 사이드바 메뉴 -->
-      <IconSideBar />
-    </div>
+    <!-- <div class="sidebar"> -->
+    <!-- 사이드바 메뉴 -->
+    <!-- <IconSideBar /> -->
+    <!-- </div> -->
 
     <div class="main-content">
+      <!-- 페이지 내용은 여기서 로드됨 -->
       <router-view />
     </div>
   </div>
@@ -17,49 +23,4 @@ import IconSideBar from "./components/IconSideBar.vue";
 import SearchBar from "./components/SearchBar.vue"; // 검색창 컴포넌트 추가
 </script>
 
-<style>
-#app {
-  display: flex;
-  height: 100vh;
-  font-family: "Arial", sans-serif;
-}
-
-.sidebar {
-  width: 3rem;
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  background-color: #f0f0f0;
-  z-index: 1000;
-}
-
-.main-content {
-  margin-left: 3rem;
-  width: calc(100vw - 3rem);
-  height: 100vh;
-  padding: 0;
-  position: relative;
-  background-color: #fff;
-  flex-grow: 1;
-}
-
-.search-bar {
-  position: absolute;
-  top: 20px;
-  left: 3.5rem; /* 검색창을 사이드바에서 3.5rem 떨어지도록 설정 */
-  z-index: 1001;
-  background-color: white;
-  padding: 10px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-}
-
-#map {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
-import './assets/main.css'
+// import "./assets/main.css";
+import "aos/dist/aos.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
+import AOS from "aos";
 
-import App from './App.vue'
-import router from './router'
+import "./styles/tailwind.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-import './styles/tailwind.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
+AOS.init();
 
-app.mount('#app')
+app.mixin({
+  mounted() {
+    AOS.init({
+      // AOS 옵션을 여기에 추가할 수 있습니다
+      duration: 800,
+      once: true,
+    });
+  },
+  updated() {
+    AOS.refresh();
+  },
+});
+
+app.mount("#app");
