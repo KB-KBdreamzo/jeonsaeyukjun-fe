dc1c829066a818b2823633d8e260a11ff1f0ea95
김시완 <rkdlqdh112@naver.com>
2024-10-02 14:32:05 +0900

    feat: KBF-12 소개페이지 탑바를 App.vue로 옮김

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,5 @@
 <template>
   <div id="app" class="flex flex-col h-screen">
-    <header
-      class="flex justify-between items-center p-2 bg-white fixed top-0 w-full z-10 border-b"
-    >
-      <div class="flex items-center ml-20">
-        <img
-          src="@/assets/logo.png"
-          alt="전세역전 로고"
-          class="w-12 h-12 mr-2"
-        />
-        <span class="text-xl font-normal">전세역전</span>
-      </div>
-      <nav class="mr-20">
-        <ul class="flex space-x-20">
-          <li>
-            <a
-              href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
-              >리포트</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
-              >지도</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
-              >계약서</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
-              >Login</a
-            >
-          </li>
-        </ul>
-      </nav>
-    </header>
-
     <main>
       <!-- 첫 번째 전체 화면 섹션 -->
       <div
