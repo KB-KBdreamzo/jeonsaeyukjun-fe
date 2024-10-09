42c9b3a218cdce001641a1ce8d92611cdd5ed85a
JOLLA99 <yoon6148@daum.net>
2024-10-09 22:28:27 +0900

    feat:KBF-22 모달창 UI 구현

---
+++ src/views/Agent/AgentView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <button @click="toggleModal">모달</button>
+      <AgentBookModal v-if="showModal" @close="toggleModal" />
+    </div>
+  </template>
+  
+  <script>
+  import AgentBookModal from '@/components/Agent/AgentBookModal.vue';
+  
+  export default {
+    components: {
+      AgentBookModal,
+    },
+    data() {
+      return {
+        showModal: false, 
+      };
+    },
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal; 
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* 여기에 스타일을 추가하세요 */
+  </style>
+  
