46178bfc6fafe4b48308cf995159156b365a568b
yoondabin <yundabin0608@gmail.com>
2024-10-10 11:06:11 +0900

    KBF-24 행정동코드 -> 법정동 코드 변환 완료

--- src/views/AgentView.vue
+++ src/views/AgentView.vue
@@ -15,7 +15,7 @@
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
               <MenuItems class="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div class="py-1">
-                  <MenuItem v-for="code in hangjungdong.sido" :key="code.sido" v-slot="{ active }">
+                  <MenuItem v-for="code in legalCode.sido" :key="code.sido" v-slot="{ active }">
                     <a href="#" @click="selectSido(code)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">{{ code.codeNm }}</a>
                   </MenuItem>
                 </div>
@@ -62,14 +62,14 @@
               </MenuItems>
             </transition>
           </Menu>
+          
           <button @click="fetchAgents" class="w-12 flex items-center bg-black rounded-md px-3 py-1">
             <img src="/src/assets/searchIconWhite.png" alt="Search" class="h-6 w-6" />
           </button>
         </div>
       </div>
-      {{ selectedSido}} {{ selectedSigugun  }} {{selectedDong}}
   
-      <div v-for="agent in agents" :key="agent.bzmn_conm" class="mb-4 p-4 border rounded-lg shadow-md">
+      <div v-for="agent in agents" :key="agent.bzmn_conm" class="flex flex-row items-center justify-between mb-4 p-4 border rounded-lg shadow-md">
         <div class="flex items-center">
           <div>
             <h2 class="font-semibold">{{ agent.bzmn_conm }}</h2>
@@ -80,6 +80,9 @@
             <p class="text-gray-600">{{ agent.telno }}</p>
           </div>
         </div>
+        <div>
+          <button @click="reserveAgent" class="flex items-center bg-black text-white rounded-md px-3 py-1"> 예약하기 </button>
+        </div>
       </div>
     </div>
   </template>
@@ -87,7 +90,7 @@
   <script setup>
   import { ref } from 'vue';
   import axios from 'axios';
-  import { hangjungdong } from '@/assets/hangjungdong.js'; // JSON 파일 경로
+  import { legalCode } from '@/assets/logalCode'; // JSON 파일 경로
   import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
   import { ChevronDownIcon } from '@heroicons/vue/20/solid';
   
@@ -124,7 +127,7 @@
   const onSidoChange = () => {
     selectedSigugun.value = '';
     selectedDong.value = '';
-    filteredSigugun.value = hangjungdong.sigugun.filter( sig => sig.sido === selectedSido.value.sido);
+    filteredSigugun.value = legalCode.sigugun.filter( sig => sig.sido === selectedSido.value.sido);
     filteredDong.value = [];
 
   };
@@ -132,16 +135,15 @@
   // 동 업데이트
   const onSigugunChange = () => {
     selectedDong.value = '';
-    filteredDong.value = hangjungdong.dong.filter(dong => 
+    filteredDong.value = legalCode.dong.filter(dong => 
       dong.sido === selectedSido.value.sido && dong.sigugun === selectedSigugun.value.sigugun
     );
   };
 
   const fetchAgents = async () => {
-    // 행정동 코드 조합
-    const legalCode = 1120011200;
-    
+    const legalCode = parseInt(selectedSido.value.sido + selectedSigugun.value.sigugun + selectedDong.value.dong + "00");
     try {
+      console.log(legalCode);
       const response = await axios.get(`http://localhost:8088/api/agent/${legalCode}`);
       console.log(response.data);
       agents.value = response.data; 
