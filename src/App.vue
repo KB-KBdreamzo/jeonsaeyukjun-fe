dbf23495c09e6c1fb148f7c76ad0dd921ddb69a7
김시완 <rkdlqdh112@naver.com>
2024-10-10 18:01:09 +0900

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import IconSideBar from "./components/IconSideBar.vue";
-</script>
-
 <template>
   <div id="app">
-    <!-- <div class="sidebar"> -->
-    <!-- 사이드바 메뉴 -->
-    <!-- <IconSideBar /> -->
-    <!-- </div> -->
     <header
       class="flex justify-between items-center p-2 bg-white fixed top-0 w-full z-10 border-b"
     >
@@ -18,47 +9,99 @@ import IconSideBar from "./components/IconSideBar.vue";
           alt="전세역전 로고"
           class="w-12 h-12 mr-2"
         />
-        <span class="text-xl font-normal">전세역전</span>
+        <span class="text-xl font-custom">전세역전</span>
       </div>
       <nav class="mr-20">
         <ul class="flex space-x-20">
           <li>
             <a
               href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
+              class="text-black font-normal hover:bg-gray-200 px-4 py-2 rounded transition duration-300"
               >리포트</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
+              class="text-black font-normal hover:bg-gray-200 px-4 py-2 rounded transition duration-300"
               >지도</a
             >
           </li>
           <li>
             <a
               href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
+              class="text-black font-normal hover:bg-gray-200 px-4 py-2 rounded transition duration-300"
               >계약서</a
             >
           </li>
           <li>
             <a
+              v-if="!userStore.isLogin"
               href="#"
-              class="text-black font-bold hover:text-gray-300 font-normal"
+              class="text-black font-normal hover:bg-gray-200 px-4 py-2 rounded transition duration-300"
+              @click.prevent="openModal"
               >Login</a
+            >
+            <router-link
+              v-else
+              to="/mypage"
+              class="text-black font-normal hover:bg-gray-200 px-4 py-2 rounded transition duration-300"
+              >Mypage</router-link
             >
           </li>
         </ul>
       </nav>
     </header>
 
-    <div class="main-content">
-      <!-- 페이지 내용은 여기서 로드됨 -->
+    <div class="main-content mt-19.5">
       <router-view />
     </div>
+
+    <LoginModal
+      :isOpen="isModalOpen"
+      @close="closeModal"
+      @kakao-login="openTermsOfUseModal"
+    />
+    <TermsOfUseModal
+      :isOpen="isTermsOfUseModalOpen"
+      @close="closeTermsOfUseModal"
+    />
   </div>
 </template>
 
-<style></style>
+<script setup>
+import { ref } from "vue";
+import { useUserStore } from "@/stores/userStore";
+import LoginModal from "./components/LoginModal.vue";
+import TermsOfUseModal from "./components/TermsOfUseModal.vue";
+
+const userStore = useUserStore();
+const isModalOpen = ref(false);
+const isTermsOfUseModalOpen = ref(false);
+
+const openModal = () => {
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
+const openTermsOfUseModal = () => {
+  isTermsOfUseModalOpen.value = true;
+};
+
+const closeTermsOfUseModal = () => {
+  isTermsOfUseModalOpen.value = false;
+};
+</script>
+
+<style scoped>
+@font-face {
+  font-family: "WavvePADO-Regular";
+  src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/2404@1.0/WavvePADO-Regular.woff2")
+    format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+</style>
