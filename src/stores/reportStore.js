ca648a0e4127706c37e6ba1c2c44a2626ed0ecb6
yoondabin <yundabin0608@gmail.com>
2024-10-11 18:27:15 +0900

    fix: KBF-27 api 수정

--- src/stores/reportStore.js
+++ src/stores/reportStore.js
@@ -5,7 +5,8 @@ import { useUserStore } from "@/stores/userStore";
 
 export const useReportStore = defineStore('reportStore', {
   state: () => ({
-    reportData: {}, 
+    reportId: 0, 
+    reportData: {},
     error: null,    
   }),
   actions: {
@@ -15,7 +16,7 @@ export const useReportStore = defineStore('reportStore', {
         const userStore = useUserStore(); 
         console.log("리포트 생성하고 저장합니다",report.deposit, report.jbAddress, report.detailedAddress, report.legalCode, registerStore.registerData)
         const response = await axios.post(
-          `http://localhost:8088/api/report/${userStore.userId}`,
+          `http://localhost:8080/api/report/${userStore.userId}`,
           {
             deposit: report.deposit,
             jbAddress: report.jbAddress,
@@ -24,11 +25,7 @@ export const useReportStore = defineStore('reportStore', {
             registerDto: registerStore.registerData
           }
         );
-        const reportId = response.data
-        console.log("저장했고 아이디 반환  조회할겁니다" + reportId)
-        const reportResponse = await axios.get(`http://localhost:8088/api/report/${reportId}`);
-        console.log(">>>리포트 조회 완료", reportResponse.data)
-        this.reportData = reportResponse.data; 
+        this.reportId = response.data
       } catch (error) {
         console.error('Error submitting report:', error);
         this.error = '리포트 제출 중 오류가 발생했습니다.';
